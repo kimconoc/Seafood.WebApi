53204fbf1082c51b88af0d31bc4e06403455b820
ducpv13 <ducpv13@vingroup.net>
2022-08-26 10:43:58 +0700

    Update

---
+++ StoreProduct.WebApi/StoreProduct.WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,36 @@
+﻿using StoreProduct.Repository.EntityFamework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace StoreProduct.WebApi.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected IUnitOfWork unitOfWork = new EfUnitOfWork();
+        protected string GetIp()
+        {
+            if (Request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)Request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            else if (Request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop = (RemoteEndpointMessageProperty)Request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            }
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
