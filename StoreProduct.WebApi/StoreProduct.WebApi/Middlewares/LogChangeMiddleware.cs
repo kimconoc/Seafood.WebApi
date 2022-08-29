193751ba99022504cb2af4923677169d442fa0fc
ducpv13 <ducpv13@vingroup.net>
2022-08-29 17:09:58 +0700

    Update api

---
+++ StoreProduct.WebApi/StoreProduct.WebApi/Middlewares/LogChangeMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace StoreProduct.WebApi.Middlewares
+{
+    public class LogChangeMiddleware : OwinMiddleware
+    {
+        public LogChangeMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            var cr_at = DateTime.Now;
+            var request_path = context.Request.Path.Value;
+            var request_method = context.Request.Method;
+            if (!request_path.Contains("api"))
+            {
+                await Next.Invoke(context);
+                return;
+            }
+
+            var log_created_by = context.Request.User.Identity.Name;
+            var log_ip = context.Request.RemoteIpAddress;
+            var log_uri = context.Request.Uri.ToString();
+            var log_action = request_method;
+            var log_request = string.Empty;
+            if (context.Request.Body.CanRead)
+            {
+                using (StreamReader reader = new StreamReader(context.Request.Body))
+                {
+                    log_request = reader.ReadToEndAsync().Result;
+                }
+            }
+            await Next.Invoke(context);
+        }
+    }
+}
---
+++ StoreProduct.WebApi/StoreProduct.WebApi/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using StoreProduct.Repository.EntityFamework;
+using StoreProduct.WebApi.App_Start;
+using StoreProduct.WebApi.Middlewares;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(StoreProduct.WebApi.Startup))]
+
+namespace StoreProduct.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable CORS (cross origin resource sharing) for making request using browser from different domains
+            //if (ConfigurationManager.AppSettings["HiddenError"].Equals("false"))
+            //    app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            //Cookie authenicate
+            var session_timeout = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["SessionTimeout"]);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = "ApplicationCookie",
+                CookieName = "EFormED",
+                SlidingExpiration = true,
+                ExpireTimeSpan = TimeSpan.FromMinutes(session_timeout),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnResponseSignedIn = context =>
+                    {
+                        var cookies = context.Response.Headers.GetCommaSeparatedValues("Set-Cookie");
+                        var cookieValue = GetAuthenCookie(cookies);
+
+                        if (!string.IsNullOrEmpty(cookieValue))
+                            UpdateSession(context.Identity.Name, cookieValue);
+                    }
+                }
+            });
+            //Log middleware
+            app.Use(typeof(LogChangeMiddleware));
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+
+        private string GetAuthenCookie(IList<string> cookies)
+        {
+            var cookieValue = "";
+
+            foreach (var cookie in cookies)
+            {
+                var cookieKeyIndex = cookie.IndexOf("EFormED");
+                if (cookieKeyIndex != -1)
+                {
+                    // Add extra character for '='
+                    cookieValue = cookie.Substring("EFormED".Length + 1);
+                    break;
+                }
+            }
+            return cookieValue;
+        }
+        private void UpdateSession(string username, string cookieValue)
+        {
+            try
+            {
+                using (var unitOfWork = new EfUnitOfWork())
+                {
+                    var user = unitOfWork.UserRepository.FirstOrDefault(m => !m.IsDeleted && m.Username == username);
+                    user.SessionId = cookieValue.Substring(0, 20);
+                    user.Session = cookieValue;
+                    unitOfWork.UserRepository.Update(user);
+                    unitOfWork.Commit();
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
