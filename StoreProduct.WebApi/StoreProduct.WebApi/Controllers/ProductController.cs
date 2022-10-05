3f84e174a849a39cb254e3dadc20ad42508bf2f1
ducpv13 <ducpv13@vingroup.net>
2022-10-05 08:30:08 +0700

    Update

--- StoreProduct.WebApi/StoreProduct.WebApi/Controllers/ProductController.cs
+++ StoreProduct.WebApi/StoreProduct.WebApi/Controllers/ProductController.cs
@@ -18,24 +18,25 @@ namespace StoreProduct.WebApi.Controllers
         {
             try
             {
-                var products = (from prod in unitOfWork.ProductRepository.AsQueryable()
-                               join favourite in unitOfWork.FavouriteProdRepository.AsQueryable() on prod.Id equals favourite.ProductId
-                               select new Product
-                               {
-                                   Id = prod.Id,
-                                   CategoryCode = prod.CategoryCode,
-                                   ImgeProdId = prod.ImgeProdId,
-                                   Name = prod.Name,
-                                   DescPromotion = prod.DescPromotion,
-                                   Description = prod.Description,
-                                   Outstanding = prod.Outstanding,
-                                   Favourite = favourite.ClassName,
-                                   Price = prod.Price,
-                                   PriceSale = prod.PriceSale,
-                                   Amount = prod.Amount
-
-                               }
-                           ).ToList();
+                var ipRequest = GetIp();
+                var products = (from prod in unitOfWork.ProductRepository.AsQueryable().Where(e => !e.IsDeleted)
+                                join favourite in unitOfWork.FavouriteProdRepository.AsQueryable().Where(e => !e.IsDeleted && e.IpRequest.Equals(ipRequest))
+                                on prod.Id equals favourite.ProductId into res
+                                from x in res.DefaultIfEmpty()
+                                select new
+                                {
+                                    Id = prod.Id,
+                                    CategoryCode = prod.CategoryCode,
+                                    ImgeProdId = prod.ImgeProdId,
+                                    Name = prod.Name,
+                                    DescPromotion = prod.DescPromotion,
+                                    Description = prod.Description,
+                                    Outstanding = prod.Outstanding,
+                                    Favourite = x.ClassName,
+                                    Price = prod.Price,
+                                    PriceSale = prod.PriceSale,
+                                    Amount = prod.Amount
+                                }).ToList();
 
                 if (products == null || products.Count() == 0)
                 {
