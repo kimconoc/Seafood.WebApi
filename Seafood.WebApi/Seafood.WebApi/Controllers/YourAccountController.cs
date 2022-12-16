4ea4fa11432ed08e48e7bf18fe8148139eb384e0
ducpv13 <ducpv13@vingroup.net>
2022-12-16 17:28:55 +0700

    push

--- Seafood.WebApi/Seafood.WebApi/Controllers/YourAccountController.cs
+++ Seafood.WebApi/Seafood.WebApi/Controllers/YourAccountController.cs
@@ -1,13 +1,7 @@
-﻿using Seafood.Domain.Common.Constant;
-using Seafood.Domain.Common.FileLog;
-using Seafood.Domain.Models.DataAccessModel;
+﻿using Seafood.Domain.Common.FileLog;
 using Seafood.WebApi.Authentication;
 using System;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Reflection;
-using System.Web;
 using System.Web.Http;
 
 namespace Seafood.WebApi.Controllers
@@ -36,33 +30,16 @@ namespace Seafood.WebApi.Controllers
 
         [HttpPost]
         [Route("api/User/UpdateAvarta")]
-        public IHttpActionResult UpdateAvarta(Guid userId, HttpPostedFileBase imgUpload)
+        public IHttpActionResult UpdateAvarta(Guid userId,string path)
         {
             try
             {
                 var user = unitOfWork.UserRepository.FirstOrDefault(x => !x.IsDeleted && !x.IsLocked && x.Id == userId);
                 if (user == null)
                     return Ok(Not_Found());
-                if (imgUpload != null)
-                {
-                    string filePath = string.Empty;
-                    filePath = System.Web.Hosting.HostingEnvironment.MapPath("~/FileUpload/seafood/avarta-user/");
-                    if (!Directory.Exists(filePath))
-                    {
-                        Directory.CreateDirectory(filePath);
-                    }
-                    filePath = filePath + Path.GetFileName(userId.ToString());
-                    imgUpload.SaveAs(filePath);
-
-                    user.Avarta = filePath;
-                    unitOfWork.UserRepository.Update(user);
-                    unitOfWork.Commit();
-
-                }
-                else
-                {
-                    return Ok(Bad_Request());
-                }
+                user.Avarta = path;
+                unitOfWork.UserRepository.Update(user);
+                unitOfWork.Commit();
                 dynamic data = true;
                 return Ok(Request_OK<dynamic>(data));
             }
