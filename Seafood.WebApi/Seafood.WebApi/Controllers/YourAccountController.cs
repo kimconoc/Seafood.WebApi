a5362855d1365cdd050a9b8a7d5f9854ab18dd21
ducpv13 <ducpv13@vingroup.net>
2022-12-20 10:40:44 +0700

    Update

--- Seafood.WebApi/Seafood.WebApi/Controllers/YourAccountController.cs
+++ Seafood.WebApi/Seafood.WebApi/Controllers/YourAccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Seafood.Domain.Common.FileLog;
+using Seafood.Domain.Models.DataAccessModel;
 using Seafood.WebApi.Authentication;
 using System;
 using System.Reflection;
@@ -41,6 +42,31 @@ namespace Seafood.WebApi.Controllers
                 unitOfWork.UserRepository.Update(user);
                 unitOfWork.Commit();
                 dynamic data = true;
+                return Ok(Request_OK<dynamic>(data));
+            }
+            catch (Exception ex)
+            {
+                FileHelper.GeneratorFileByDay(ex.ToString(), MethodBase.GetCurrentMethod().Name);
+                return Ok(Server_Error());
+            }
+        }
+        [HttpPost]
+        [Route("api/User/UploadProfile")]
+        public IHttpActionResult UploadProfile(User userData)
+        {
+            try
+            {
+                var user = unitOfWork.UserRepository.FirstOrDefault(x => !x.IsDeleted && !x.IsLocked && x.Id == userData.Id);
+                if (user == null)
+                    return Ok(Not_Found());
+                user.DisplayName = userData.DisplayName;
+                user.Birthday = userData.Birthday;
+                user.Sex = userData.Sex;
+                user.Email = userData.Email;
+                user.Mobile = userData.Mobile.Trim();
+                unitOfWork.UserRepository.Update(user);
+                unitOfWork.Commit();
+                dynamic data = user;
                 return Ok(Request_OK<dynamic>(data));
             }
             catch (Exception ex)
